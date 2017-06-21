be7c8e4768daf493115baa58dc5dc51954f51d87
ApplePie3323 <lmika3323@gmail.com>
2017-06-20 15:00:09 -1000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+function eatHumans(bearName, humanNames){
+//console.log(bearName);
+//console.log(humanNames);
+console.log(humanNames[0] + " was eaten by " + bearName + " , " + humanNames[1] + " was not eaten by " + bearName + " , " + humanNames[2] + " was eaten by " + bearName + ".")
+}
+
+eatHumans("Moron", ["Michelle", "Mika", "Jimmeh"]);
